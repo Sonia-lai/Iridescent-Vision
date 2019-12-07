825c7ea842b1026ff067cca5b9c5b0e6ba31d784
lojoyu <sophie200429@gmail.com>
2019-12-07 22:47:26 +0800

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 
 module.exports = {
     entry: "./src/app.js",
-    //entry: "./src/transparent.js",
     performance: {
         hints: false,
         maxEntrypointSize: 512000,
